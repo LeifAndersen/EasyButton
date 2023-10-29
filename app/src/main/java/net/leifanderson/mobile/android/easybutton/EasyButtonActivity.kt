026048ad82dc5a188b24a37db524efbd0b8b336f
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2023-10-29 03:22:46 -0400

    Ask for battery permissions

--- app/src/main/java/net/leifanderson/mobile/android/easybutton/EasyButtonActivity.kt
+++ app/src/main/java/net/leifanderson/mobile/android/easybutton/EasyButtonActivity.kt
@@ -1,19 +1,18 @@
 package net.leifanderson.mobile.android.easybutton
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import net.leifanderson.mobile.android.easybutton.ui.theme.EasyButtonTheme
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+
 
 class EasyButtonActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +22,21 @@ class EasyButtonActivity : ComponentActivity() {
         (findViewById<View>(R.id.easy_button_layout) as ImageButton).setOnClickListener {
             val leMediaPlayer = MediaPlayer.create(this@EasyButtonActivity.applicationContext, R.raw.easy)
             leMediaPlayer.start()
-            leMediaPlayer.setOnCompletionListener{
+            leMediaPlayer.setOnCompletionListener {
                 leMediaPlayer.release()
             }
+        }
+
+        if(ContextCompat.checkSelfPermission(this@EasyButtonActivity, Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS) != PackageManager.PERMISSION_GRANTED) { // I have found that with the new API target, sometimes the app will be "optimized" and the service for the media playback won't start reliably without removing
+            // background restriction
+            AlertDialog.Builder(this@EasyButtonActivity).setMessage("For the widget to reliably work, you need to grant background permissions.").setTitle("Permission needed").setPositiveButton("Okee, I will enably") { dialog, which ->
+                    val myIntent = Intent()
+                    myIntent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+                    myIntent.data = Uri.parse("package:" + this.packageName)
+                    startActivity(myIntent)
+                }.setNegativeButton("Nah, I don't care") { dialog, which ->
+                    AlertDialog.Builder(this@EasyButtonActivity).setMessage("The widget may or may not work properly!").setTitle("You were warned!").setPositiveButton("I'll take my chances") { _, _ ->/*Do nothing*/ }.show()
+                }.show()
         }
     }
 }
