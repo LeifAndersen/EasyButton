1a3cdacfcd9f93de2103a64231d545d383d989b2
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2023-10-29 03:09:24 -0400

    More cleanup

--- app/src/main/java/net/leifanderson/mobile/android/easybutton/EasyButton.java
+++ app/src/main/java/net/leifanderson/mobile/android/easybutton/EasyButton.java
@@ -7,20 +7,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
-public class EasyButton extends AppWidgetProvider {
-  @Override
-  public void onUpdate(Context paramContext, AppWidgetManager paramAppWidgetManager, int[] paramArrayOfint) {
-    int j = paramArrayOfint.length;
-    int i;
-    for (i = 0;; i++) {
-      if(i >= j){
-        return;
-      }
-      int k = paramArrayOfint[i];
-      PendingIntent pendingIntent = PendingIntent.getService(paramContext, 0, new Intent(paramContext, EasyButtonService.class), PendingIntent.FLAG_IMMUTABLE);
-      RemoteViews remoteViews = new RemoteViews(paramContext.getPackageName(), R.layout.easy_button_layout);
-      remoteViews.setOnClickPendingIntent(R.id.easy_button_layout, pendingIntent);
-      paramAppWidgetManager.updateAppWidget(k, remoteViews);
-    } 
-  }
+public class EasyButton extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context paramContext, AppWidgetManager paramAppWidgetManager, int[] paramArrayOfint){
+        int j = paramArrayOfint.length;
+        int i;
+        for(i = 0; ; i++){
+            if(i >= j){
+                return;
+            }
+            int k = paramArrayOfint[i];
+            PendingIntent pendingIntent = PendingIntent.getService(paramContext, 0, new Intent(paramContext, EasyButtonService.class), PendingIntent.FLAG_IMMUTABLE);
+            RemoteViews remoteViews = new RemoteViews(paramContext.getPackageName(), R.layout.easy_button_layout);
+            remoteViews.setOnClickPendingIntent(R.id.easy_button_layout, pendingIntent);
+            paramAppWidgetManager.updateAppWidget(k, remoteViews);
+        }
+    }
 }
--- app/src/main/java/net/leifanderson/mobile/android/easybutton/EasyButtonSmall.java
+++ app/src/main/java/net/leifanderson/mobile/android/easybutton/EasyButtonSmall.java
@@ -7,20 +7,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
-public class EasyButtonSmall extends AppWidgetProvider {
-  @Override
-  public void onUpdate(Context paramContext, AppWidgetManager paramAppWidgetManager, int[] paramArrayOfint) {
-    int j = paramArrayOfint.length;
-    int i;
-    for (i = 0;; i++) {
-      if(i >= j){
-        return;
-      }
-      int k = paramArrayOfint[i];
-      PendingIntent pendingIntent = PendingIntent.getService(paramContext, 0, new Intent(paramContext, EasyButtonService.class), PendingIntent.FLAG_IMMUTABLE);
-      RemoteViews remoteViews = new RemoteViews(paramContext.getPackageName(), R.layout.easy_button_layout_small);
-      remoteViews.setOnClickPendingIntent(R.id.easy_button_layout_small, pendingIntent);
-      paramAppWidgetManager.updateAppWidget(k, remoteViews);
-    } 
-  }
+public class EasyButtonSmall extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context paramContext, AppWidgetManager paramAppWidgetManager, int[] paramArrayOfint){
+        int j = paramArrayOfint.length;
+        int i;
+        for(i = 0; ; i++){
+            if(i >= j){
+                return;
+            }
+            int k = paramArrayOfint[i];
+            PendingIntent pendingIntent = PendingIntent.getService(paramContext, 0, new Intent(paramContext, EasyButtonService.class), PendingIntent.FLAG_IMMUTABLE);
+            RemoteViews remoteViews = new RemoteViews(paramContext.getPackageName(), R.layout.easy_button_layout_small);
+            remoteViews.setOnClickPendingIntent(R.id.easy_button_layout_small, pendingIntent);
+            paramAppWidgetManager.updateAppWidget(k, remoteViews);
+        }
+    }
 }
